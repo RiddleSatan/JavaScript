bc42fbb97b773dc5819bacacf566ae0afe9d8314
Om Pandey <ompandey007j@gmail.com>
2023-12-22 16:27:51 +0530

    obj notes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,7 +24,7 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //Object.assign() modifies the target object directly, not creating a new one.
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
